22a0178a8a95c95c853eef09a0944bfe64e08be8
Tech Tonic <chavezjonas347@gmail.com>
2025-10-07 13:05:07 -0500

    Delete gestión directory

--- gestión/src/main/java/pe/edu/upeu/chavez/gestion/repository/ClienteRepository.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upeu.chavez.gestion.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pe.edu.upeu.chavez.gestion.entity.Cliente;
-
-@Repository
-public interface ClienteRepository extends JpaRepository<Cliente, Long> {
-}
--- gestión/src/main/java/pe/edu/upeu/chavez/gestion/repository/ParticipaRepository.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upeu.chavez.gestion.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pe.edu.upeu.chavez.gestion.entity.Participa;
-
-@Repository
-public interface ParticipaRepository extends JpaRepository<Participa, Long> {
-}
